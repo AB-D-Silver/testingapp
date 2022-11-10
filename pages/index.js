4f6dd40fee6cfc4d8fa2aa8020acddc204af992b
Ankit Bisht <ankitbisht0301@gmail.com>
2022-11-10 15:46:31 +0530

    first commit only for Testing purpose

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+
+import { TextStyle } from '@shopify/polaris';
+
+const Index = () => (
+    <div>
+        <TextStyle variation="positive">Example Shopify App</TextStyle>
+    </div>
+);
+
+export default Index;
